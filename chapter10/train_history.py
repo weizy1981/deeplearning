05ba3dec77ed4a45f9215e95d197555666871d17
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-09-21 21:26:41 +0800

    chapter10

--- chapter10/train_history.py
+++ chapter10/train_history.py
@@ -3,6 +3,8 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
+from matplotlib import pyplot as plt
+
 
 # 导入数据
 dataset = datasets.load_iris()
@@ -17,16 +19,12 @@ Y_labels = to_categorical(Y, num_classes=3)
 seed = 7
 np.random.seed(seed)
 # 构建模型函数
-def load_model(optimizer='rmsprop', init='glorot_uniform'):
+def create_model(optimizer='rmsprop', init='glorot_uniform'):
     # 构建模型
     model = Sequential()
     model.add(Dense(units=4, activation='relu', input_dim=4, kernel_initializer=init))
     model.add(Dense(units=6, activation='relu', kernel_initializer=init))
     model.add(Dense(units=3, activation='softmax', kernel_initializer=init))
-
-    # 加载权重
-    filepath = 'weights.best.h5'
-    model.load_weights(filepath=filepath)
 
     # 编译模型
     model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
@@ -34,9 +32,31 @@ def load_model(optimizer='rmsprop', init='glorot_uniform'):
     return model
 
 # 构建模型
-model = load_model()
+model = create_model()
+
+history = model.fit(x, Y_labels, validation_split=0.2, epochs=200, batch_size=5, verbose=0)
 
 # 评估模型
 scores = model.evaluate(x, Y_labels, verbose=0)
 print('%s: %.2f%%' % (model.metrics_names[1], scores[1] * 100))
 
+# Hisotry列表
+print(history.history.keys())
+
+# accuracy的历史
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+# loss的历史
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
