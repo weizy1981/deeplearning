d2bcabd6a1b5f8026003d849ab4ad39313fe52c5
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-09-21 08:19:00 +0800

    chapter10 start

---
+++ chapter10/checkpoint_best.py
@@ -0,0 +1,42 @@
+from sklearn import datasets
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+from matplotlib import pyplot as plt
+
+# 导入数据
+dataset = datasets.load_iris()
+
+x = dataset.data
+Y = dataset.target
+
+# Convert labels to categorical one-hot encoding
+Y_labels = to_categorical(Y, num_classes=3)
+
+# 设定随机种子
+seed = 7
+np.random.seed(seed)
+# 构建模型函数
+def create_model(optimizer='rmsprop', init='glorot_uniform'):
+    # 构建模型
+    model = Sequential()
+    model.add(Dense(units=4, activation='relu', input_dim=4, kernel_initializer=init))
+    model.add(Dense(units=6, activation='relu', kernel_initializer=init))
+    model.add(Dense(units=3, activation='softmax', kernel_initializer=init))
+
+    # 编译模型
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    return model
+
+# 构建模型
+model = create_model()
+
+# 设置检查点
+filepath = 'weights.best.h5'
+checkpoint = ModelCheckpoint(filepath=filepath, monitor='val_acc', verbose=1, save_weights_only=True, mode='max')
+callback_list = [checkpoint]
+model.fit(x, Y_labels, validation_split=0.2, epochs=200, batch_size=5, verbose=0, callbacks=callback_list)
+
