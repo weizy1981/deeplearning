149501d9cec2d8b1605c13ac3bfe616591c832d3
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-10-13 06:43:36 +0800

    chapter15

---
+++ chapter15/cnn_LeNet-5.py
@@ -0,0 +1,86 @@
+import keras
+import numpy as np
+from keras import optimizers
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from keras.callbacks import LearningRateScheduler, TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+
+batch_size = 128
+epochs = 200
+iterations = 391
+num_classes = 10
+weight_decay = 0.0001
+mean = [125.307, 122.95, 113.865]
+std = [62.9932, 62.0887, 66.7048]
+
+log_filepath = './lenet_dp_da_wd'
+
+
+def build_model():
+    model = Sequential()
+    model.add(Conv2D(6, (5, 5), padding='valid', activation='relu', kernel_initializer='he_normal',
+                     kernel_regularizer=l2(weight_decay), input_shape=(32, 32, 3)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Conv2D(16, (5, 5), padding='valid', activation='relu', kernel_initializer='he_normal',
+                     kernel_regularizer=l2(weight_decay)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(120, activation='relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay)))
+    model.add(Dense(84, activation='relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay)))
+    model.add(Dense(10, activation='softmax', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay)))
+    sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+
+def scheduler(epoch):
+    if epoch <= 60:
+        return 0.05
+    if epoch <= 120:
+        return 0.01
+    if epoch <= 160:
+        return 0.002
+    return 0.0004
+
+
+if __name__ == '__main__':
+
+    # load data
+    (x_train, y_train), (x_validation, y_validation) = cifar10.load_data()
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_validation = keras.utils.to_categorical(y_validation, num_classes)
+    x_train = x_train.astype('float32')
+    x_validation = x_validation.astype('float32')
+
+    # data preprocessing  [raw - mean / std]
+    for i in range(3):
+        x_train[:, :, :, i] = (x_train[:, :, :, i] - mean[i]) / std[i]
+        x_validation[:, :, :, i] = (x_validation[:, :, :, i] - mean[i]) / std[i]
+
+    # build network
+    model = build_model()
+    print(model.summary())
+
+    # set callback
+    tb_cb = TensorBoard(log_dir=log_filepath, histogram_freq=0)
+    change_lr = LearningRateScheduler(scheduler)
+    cbks = [change_lr, tb_cb]
+
+    # using real-time data augmentation
+    print('Using real-time data augmentation.')
+    datagen = ImageDataGenerator(horizontal_flip=True,
+                                 width_shift_range=0.125, height_shift_range=0.125, fill_mode='constant', cval=0.)
+
+    datagen.fit(x_train)
+
+    # start traing
+    model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                        steps_per_epoch=iterations,
+                        epochs=epochs,
+                        callbacks=cbks,
+                        validation_data=(x_validation, y_validation))
+    # save model
+    model.save('lenet.h5')
