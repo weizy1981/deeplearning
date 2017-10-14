899f79de6d6e53af609ba22676e0861501561d25
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-10-14 09:26:33 +0800

    chapter15

--- chapter15/cnn_network_in_network.py
+++ chapter15/cnn_network_in_network.py
@@ -1,14 +1,12 @@
 import keras
 import numpy as np
 from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, AveragePooling2D
+from keras.layers import Dropout, Activation
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from keras.initializers import RandomNormal
 from keras import optimizers
 from keras.callbacks import LearningRateScheduler, TensorBoard
-from keras.layers.normalization import BatchNormalization
 
 batch_size = 128
 epochs = 200
@@ -18,16 +16,16 @@ dropout = 0.5
 log_filepath = './nin'
 
 
-def color_preprocessing(x_train, x_test):
+def normalize_preprocessing(x_train, x_validation):
     x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
+    x_validation = x_validation.astype('float32')
     mean = [125.307, 122.95, 113.865]
     std = [62.9932, 62.0887, 66.7048]
     for i in range(3):
         x_train[:, :, :, i] = (x_train[:, :, :, i] - mean[i]) / std[i]
-        x_test[:, :, :, i] = (x_test[:, :, :, i] - mean[i]) / std[i]
+        x_validation[:, :, :, i] = (x_validation[:, :, :, i] - mean[i]) / std[i]
 
-    return x_train, x_test
+    return x_train, x_validation
 
 
 def scheduler(epoch):
@@ -44,56 +42,49 @@ def build_model():
     model = Sequential()
 
     model.add(Conv2D(192, (5, 5), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.01), input_shape=x_train.shape[1:]))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.01), input_shape=x_train.shape[1:],
+                     activation='relu'))
     model.add(Conv2D(160, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(Conv2D(96, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same'))
 
     model.add(Dropout(dropout))
 
     model.add(Conv2D(192, (5, 5), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(Conv2D(192, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(Conv2D(192, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same'))
 
     model.add(Dropout(dropout))
 
     model.add(Conv2D(192, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(Conv2D(192, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
     model.add(Conv2D(10, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(0.0001),
-                     kernel_initializer=RandomNormal(stddev=0.05)))
-    model.add(Activation('relu'))
+                     kernel_initializer=RandomNormal(stddev=0.05), activation='relu'))
 
     model.add(GlobalAveragePooling2D())
     model.add(Activation('softmax'))
 
-    sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True)
+    sgd = optimizers.SGD(lr=0.1, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return model
 
 
 if __name__ == '__main__':
+    np.random.seed(seed=7)
     # load data
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    (x_train, y_train), (x_validation, y_validation) = cifar10.load_data()
     y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+    y_validation = keras.utils.to_categorical(y_validation, num_classes)
 
-    x_train, x_test = color_preprocessing(x_train, x_test)
+    x_train, x_validation = normalize_preprocessing(x_train, x_validation)
 
     # build network
     model = build_model()
@@ -104,13 +95,18 @@ if __name__ == '__main__':
     change_lr = LearningRateScheduler(scheduler)
     cbks = [change_lr, tb_cb]
 
+    '''
     # set data augmentation
     print('Using real-time data augmentation.')
+    from keras.preprocessing.image import ImageDataGenerator
     datagen = ImageDataGenerator(horizontal_flip=True, width_shift_range=0.125, height_shift_range=0.125,
                                  fill_mode='constant', cval=0.)
     datagen.fit(x_train)
 
     # start training
     model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size), steps_per_epoch=iterations,
-                        epochs=epochs, callbacks=cbks, validation_data=(x_test, y_test), verbose=2)
+                        epochs=epochs, callbacks=cbks, validation_data=(x_validation, y_validation), verbose=2)
+    '''
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=cbks,
+              validation_data=(x_validation, y_validation), verbose=2)
     model.save('nin.h5')
